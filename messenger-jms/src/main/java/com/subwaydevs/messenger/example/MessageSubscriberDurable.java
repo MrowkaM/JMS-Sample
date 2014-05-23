82546f80ac7736616d355a15fe6fd44c0137e514
Malgorzata Mrowka <jadczak.malgorzata@gmail.com>
2014-05-23 12:27:42 +0200

    Examples moved to proper package; Producer producing 1000 mssgs added

---
+++ messenger-jms/src/main/java/com/subwaydevs/messenger/example/MessageSubscriberDurable.java
@@ -0,0 +1,40 @@
+package com.subwaydevs.messenger.example;
+
+import java.util.Properties;
+
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSConsumer;
+import javax.jms.JMSContext;
+import javax.jms.Topic;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class MessageSubscriberDurable {
+
+	public static void main(String[] args) throws NamingException {
+		subscribeMessage(2);
+	}
+	
+	private static void subscribeMessage(int subscriberNumber) throws NamingException {
+		 InitialContext context = configureContext();
+		ConnectionFactory connectionFactory = (ConnectionFactory) context
+				.lookup("jms/RemoteConnectionFactory");
+		Topic messages = (Topic) context.lookup("java:jms/topic/messagesTopic");
+		try (JMSContext jmsContext = connectionFactory.createContext("sdevs", "sdevs")) {
+			jmsContext.setClientID("Subscription");
+			JMSConsumer consumer = jmsContext.createDurableConsumer(messages, "Subscription");
+			System.out.println("Subscriber : " + subscriberNumber + " recieved message " + consumer.receiveBody(String.class));
+		}
+	}
+	
+	
+	private static InitialContext configureContext() throws NamingException {
+		final Properties p = new Properties();
+		p.put(Context.INITIAL_CONTEXT_FACTORY,
+				"org.jboss.naming.remote.client.InitialContextFactory");
+		p.put(Context.PROVIDER_URL, "http-remoting://127.0.0.1:8080");
+		InitialContext context = new InitialContext(p);
+		return context;
+	}
+}
---
+++ messenger-jms/src/main/java/com/subwaydevs/messenger/example/MesssageConsumer.java
@@ -0,0 +1,42 @@
+package com.subwaydevs.messenger.example;
+
+import java.util.Properties;
+
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSConsumer;
+import javax.jms.JMSContext;
+import javax.jms.Queue;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class MesssageConsumer {
+	
+	public static void main(String[] args) throws NamingException {
+		consumeMessage();
+	}
+	
+	public static void consumeMessage() throws NamingException {
+		InitialContext context = configureContext();
+		Queue messages = (Queue) context.lookup("java:jms/queue/messagesQueue");
+		ConnectionFactory connectionFactory = (ConnectionFactory) context
+				.lookup("jms/RemoteConnectionFactory");
+		try (JMSContext jmsContext = connectionFactory.createContext("sdevs", "sdevs")) {
+			JMSConsumer consumer = jmsContext.createConsumer(messages);
+			String mssg = consumer.receiveBody(String.class);
+			while(mssg != null) {
+				System.out.println(mssg);
+				mssg = consumer.receiveBody(String.class);
+			}
+		}
+	}
+	
+	private static InitialContext configureContext() throws NamingException {
+		final Properties p = new Properties();
+		p.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.naming.remote.client.InitialContextFactory");
+		p.put(Context.PROVIDER_URL, "http-remoting://127.0.0.1:8080");
+		InitialContext context = new InitialContext(p);
+		return context;
+	}
+
+}
